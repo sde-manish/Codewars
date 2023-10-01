6fa9ea2d264047449af25dfae47d372fdc55153f
Manish Kumar <manish481181@gmail.com>
2023-10-01 22:07:46 +0530

    Split strings

---
+++ 6-kyu/Split Strings.js
@@ -0,0 +1,20 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string
+// contains an odd number of characters then it should replace the missing second character of the
+//  final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str){
+    if(str.length%2 != 0){
+      str = str + '_';
+    }
+    let stringArray = [];
+    for(let i=0;i<str.length;i=i+2){
+      let a = str.slice(i,i+2);
+      stringArray.push(a);
+    }
+     return stringArray;
+  }
