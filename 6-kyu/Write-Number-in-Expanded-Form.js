870ecb5e31f7f15a67decb5d8a1571bd4b38b620
Manish Kumar <manish481181@gmail.com>
2023-10-09 23:46:43 +0530

    Update Write-Number-in-Expanded-Form.js

--- 6-kyu/Write-Number-in-Expanded-Form.js
+++ 6-kyu/Write-Number-in-Expanded-Form.js
@@ -7,30 +7,35 @@
 // expandedForm(70304); // Should return '70000 + 300 + 4'
 // NOTE: All numbers will be whole numbers greater than 0.
 
-// function expandedForm(num) {
 
-//     let ExpandedNumber = ''
-//     while (num > 0) {
-//         let numString = num.toString();
+// First solution: 
 
-//         let temp  = numString[0]
-//         for(let j = 0; j < numString.length - 1; j++){
-//             temp += '0'
+function expandedForm(num) {
+
+    let ExpandedNumber = ''
+    while (num > 0) {
+      if( ExpandedNumber !== ''){
+         ExpandedNumber += ' + '
+      }
+        let numString = num.toString();
+
+        let temp  = numString[0]
+        for(let j = 0; j < numString.length - 1; j++){
+            temp += '0'
         
-//         }
-//         if (temp.length == 1) {
-//             ExpandedNumber += temp
-//             break;   
-//         }
-//         ExpandedNumber += temp + ' + '
-//         num = num - parseInt(temp)
-    
-//     }
-//     console.log(ExpandedNumber);
+        } 
+        ExpandedNumber += temp
+        num = num - parseInt(temp)   
+    }
+       
+    console.log(ExpandedNumber);
+    return ExpandedNumber
+ 
+}
+expandedForm(67171780)
 
-//   }
-//   expandedForm(420370022)
 
+// Second solution:
 
 function expandedForm(num){
     let numArray = String(num).split('').map( (num) => {
@@ -40,24 +45,21 @@ function expandedForm(num){
 
     let Result = ''
     for(let i = 0; i < numArray.length; i++){
-        if (numArray[i] > 0) {
+        if (numArray[i] !== 0) {
+            if (Result !== '') {
+                Result += ' + '
+                
+            }
+            
             Result += numArray[i]
             for(let j = 1; j < numArray.length  - i; j++){
                 Result += '0'
             }
-            // if (i === numArray.length - 1 || Result == num) {
-            //    break
-            //    return Result
-            // }
-            for(let k = i; k < numArray.length; k++){
-                if (numArray[k + 1] !== 0 ) {
-                    Result += ' + '
-                    break
-                }
             }
             
         }
+        console.log(Result);
+    return Result
     }
-    console.log(Result);
-}
+        
 expandedForm(67171780)
