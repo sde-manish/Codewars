5edd5c3b61d0b6e6e21d4690a316e1a04e92a08a
Manish Kumar <manish481181@gmail.com>
2023-10-15 23:36:23 +0530

    Update Pete-the-baker.js

--- 5-kyu/Pete-the-baker.js
+++ 5-kyu/Pete-the-baker.js
@@ -37,16 +37,15 @@
 //     { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
 //   ));
 
-// function cakes(recipe, available) {
-//     return Object.keys(recipe).reduce(function (val, ingredient) {
-//       return Math.min(
-//         Math.floor(available[ingredient] / recipe[ingredient] || 0),
-//         val
-//       )
-//     }, Infinity)
-//   }
-//   console.log(cakes(
-//     { flour: 500, sugar: 200, eggs: 1 },
-//     { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
-//   ));
 
+
+function cakes(recipe, available) {
+    return Object.keys(recipe).reduce(function (val, ingredient){
+        return Math.min(Math.floor(available[ingredient] / recipe[ingredient] || 0), val)
+    }, Infinity)
+  }
+
+  console.log(cakes(
+        { flour: 500, sugar: 200, eggs: 1 },
+        { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+      ));
